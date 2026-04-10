1c688e91c535e89f0bf8268be5909dcdc9191d66
Threshu <szymon.banach.dev@gmail.com>
2026-04-10 23:38:35 +0200

    chore: add ESLint configuration and integrate Prettier with Tailwind CSS support

--- app.vue
+++ app.vue
@@ -1,15 +1,22 @@
 <template>
-	<div>
-		<NuxtRouteAnnouncer />
-		<!-- <NuxtWelcome /> -->
-		<Button variant="destructive" @click="console.log('test')">Test</Button>
+  <div>
+    <NuxtRouteAnnouncer />
+    <!-- <NuxtWelcome /> -->
+    <Button variant="destructive" @click="console.log('test')">Test</Button>
 
-		<button @click="$colorMode.preference = 'dark'">Dark</button>
-		<button @click="$colorMode.preference = 'light'">Light</button>
+    <button @click="$colorMode.preference = 'dark'">Dark</button>
+    <button @click="$colorMode.preference = 'light'">Light</button>
 
-		<button @click="$i18n.locale = 'en'">En</button>
-		<button @click="$i18n.locale = 'pl'">Pl</button>
+    <button @click="$i18n.locale = 'en'">En</button>
+    <button @click="$i18n.locale = 'pl'">Pl</button>
 
-		{{ $t("welcome") }}
-	</div>
+    <div :class="cn('bg-red-500 p-3 font-bold text-white')">test 2</div>
+    {{ $t("welcome") }}
+  </div>
 </template>
+
+<script setup lang="ts">
+import { cn } from "./lib/utils";
+
+const classes = cn("bg-red-500 p-3 font-bold text-white");
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,7 @@ export default defineNuxtConfig({
 		"@nuxtjs/color-mode",
 		"@nuxtjs/i18n",
 		"shadcn-nuxt",
+		"@nuxt/eslint",
 	],
 	colorMode: {
 		classSuffix: "",
